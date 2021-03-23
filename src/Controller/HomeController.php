355687286ea70628f2daa7d8ecada3b7e7116e6c
pllorca <p.llorca.pl@gmail.com>
2021-03-23 16:52:46 +0100

    portfolio1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProjetsRepository;
+use App\Repository\CompetencesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,12 +13,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(ProjetsRepository $projetsRepository): Response
+    public function index(ProjetsRepository $projetsRepository, CompetencesRepository $competencesRepository): Response
     {
         $projets = $projetsRepository->findLastSix();
+        $competences = $competencesRepository->findAll();
         
         return $this->render('home/index.html.twig', [
             'projets' => $projets,
+            'competences' => $competences,
         ]);
     }
 }
